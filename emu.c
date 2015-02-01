9b5523eb8dc6f5d63ffb45b50f24e8f9c32094e2
whitequark <whitequark@whitequark.org>
2015-02-01 17:01:35 +0300

    working single-stepping

--- emu.c
+++ emu.c
@@ -16,16 +16,13 @@
 #include <asm-generic/errno.h>
 #include <elf.h>
 
-// from mach/i386/pmap.h
-typedef uint64_t  pmap_paddr_t;
-
 #define DEBUG1(str) fprintf(stderr, "d: %s: " str "\n", __func__)
 #define DEBUG(fmt, ...) fprintf(stderr, "d: %s: " fmt "\n", __func__, __VA_ARGS__)
 #define FAIL(reason, retval) do { fail(__func__, reason, retval); goto fail; } while(0)
 #define XFAIL(reason, retval) fail(__func__, "cleanup:" reason, retval)
 #define PFAIL(reason) do { fprintf(stderr, "e: %s: %s\n", __func__, reason); goto fail; } while(0)
-#define UFAIL(reason, retval) do { fprintf(stderr, "e: %s: %s: %s\n", __func__, reason, strerror(retval)); goto fail; } while(0)
-#define XUFAIL(reason, retval) fprintf(stderr, "e: %s: cleanup: %s: %s\n", __func__, reason, strerror(retval))
+#define UFAIL(reason) do { fprintf(stderr, "e: %s: %s: %s\n", __func__, reason, strerror(errno)); goto fail; } while(0)
+#define XUFAIL(reason) fprintf(stderr, "e: %s: cleanup: %s: %s\n", __func__, reason, strerror(errno))
 #define ASSERT(expr) do { if(!(expr)) { fprintf(stderr, "e: %s: failed: %s\n", __func__, #expr); goto fail; } } while(0)
 
 #define PRINT_STATE(state) \
@@ -107,23 +104,20 @@ fail:
 /* spawn a new suspended task */
 static kern_return_t emu_spawn(mach_port_t exn, mach_port_t *task) {
         kern_return_t retval, xretval;
-        mach_port_t recv = 0, send = 0, reply = 0, child = 0;
-        mach_msg_type_name_t type;
+        mach_port_t recv = 0, reply = 0, child = 0;
 
         if((retval = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &recv)))
                 FAIL("mach_port_allocate", retval);
 
-        if((retval = mach_port_extract_right(mach_task_self(),
-                        recv, MACH_MSG_TYPE_MAKE_SEND, &send, &type)))
-                FAIL("mach_port_extract_right", retval);
-
-        // DEBUG("recv %d send %d type 0x%x", recv, send, type);
+        if((retval = mach_port_insert_right(mach_task_self(),
+                        recv, recv, MACH_MSG_TYPE_MAKE_SEND)))
+                FAIL("mach_port_insert_right", retval);
 
         mach_port_t old_bootstrap = 0;
         if((retval = task_get_bootstrap_port(mach_task_self(), &old_bootstrap)))
                 FAIL("task_get_bootstrap_port", retval);
 
-        if((retval = task_set_bootstrap_port(mach_task_self(), send)))
+        if((retval = task_set_bootstrap_port(mach_task_self(), recv)))
                 FAIL("task_set_bootstrap_port", retval);
 
         pid_t pid;
@@ -184,31 +178,30 @@ static kern_return_t emu_exn_wait(mach_port_t exn_set);
 
 static kern_return_t emu_blank_slate(mach_port_t exn_set, task_t *task, thread_t *thread) {
         kern_return_t retval, xretval;
-        mach_port_t exn = 0, exn_send = 0;
-        mach_msg_type_name_t type;
+        mach_port_t exn = 0;
         thread_array_t threads = NULL;
-        mach_msg_type_number_t thread_count;
+        mach_msg_type_number_t thread_count = 0;
 
         if((retval = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &exn)))
                 FAIL("mach_port_allocate", retval);
 
-        if((retval = mach_port_extract_right(mach_task_self(),
-                        exn, MACH_MSG_TYPE_MAKE_SEND, &exn_send, &type)))
-                FAIL("mach_port_extract_right", retval);
+        if((retval = mach_port_insert_right(mach_task_self(),
+                        exn, exn, MACH_MSG_TYPE_MAKE_SEND)))
+                FAIL("mach_port_insert_right", retval);
 
         //DEBUG("exn port %d", exn);
 
         if((retval = emu_spawn(exn, task)))
                 FAIL("emu_spawn", retval);
 
-        if((retval = task_threads(*task, &threads, &thread_count)))
-                FAIL("task_threads", retval);
-
         if((retval = emu_exn_wait(exn)))
                 FAIL("emu_exn_wait", retval);
 
         if((retval = mach_port_move_member(mach_task_self(), exn, exn_set)))
                 FAIL("mach_port_move_member", retval);
+
+        if((retval = task_threads(*task, &threads, &thread_count)))
+                FAIL("task_threads", retval);
 
         *thread = threads[0];
 
@@ -231,12 +224,10 @@ typedef struct {
         mach_port_t sysret_port;
         mach_port_t task;
         mach_port_t thread;
+        x86_thread_state64_t *state;
 } syscall_context_t;
 
-static void emu_syscall_return(syscall_context_t *context, uint64_t syscall_retval,
-                               x86_thread_state64_t *state);
-static void emu_syscall_return_nostate(syscall_context_t *context,
-                                       uint64_t syscall_retval);
+static kern_return_t emu_exn_return(syscall_context_t *context);
 
 static inline void* emu_map(syscall_context_t *context,
                             uint64_t ptr, uint64_t len, vm_prot_t prot) {
@@ -407,6 +398,15 @@ static uint64_t emu_write(syscall_context_t *context,
         return ret;
 }
 
+static uint64_t emu_mmap(syscall_context_t *context,
+                         uint64_t start, uint64_t len, uint64_t prot,
+                         uint64_t flags, uint64_t fd, uint64_t off) {
+        DEBUG("start %016llx len %lld prot %08llx flags %08llx fd %lld off %08llx",
+              start, len, prot, flags, fd, off);
+
+        return -linux_ENOSYS;
+}
+
 static uint64_t emu_exit(syscall_context_t *context,
                          uint64_t code) {
         DEBUG("code %lld", code);
@@ -436,28 +436,34 @@ static uint64_t emu_exit(syscall_context_t *context,
                 state->__rdi, state->__rsi, state->__rdx, \
                 state->__rcx, state->__r8, state->__r9); break;
 
-static boolean_t emu_syscall(syscall_context_t *context, x86_thread_state64_t *state) {
+static boolean_t emu_syscall(syscall_context_t *context) {
         uint64_t retval = 0;
+        x86_thread_state64_t *state = context->state;
 
         switch(state->__rax) {
                 SYSCALL3(write);
+                //SYSCALL6(mmap);
                 SYSCALL1(exit);
 
         /* pseudo-syscall indicating macho stub handoff */
         case 0xffff:
                 if((retval = thread_suspend(context->thread)))
-                        return FALSE;
+                        FAIL("thread_suspend", retval);
                 break;
 
         default:
-                DEBUG("unknown syscall %lld, aborting", state->__rax);
-                return FALSE;
+                DEBUG("unknown syscall %lld", state->__rax);
+                goto fail;
         }
 
-        if(retval != SYSCALL_SUSPEND)
-                emu_syscall_return(context, retval, state);
+        if(retval != SYSCALL_SUSPEND) {
+                state->__rax = retval;
+                emu_exn_return(context);
+        }
 
         return TRUE;
+
+fail:   return FALSE;
 }
 
 #undef SYSCALL1
@@ -466,6 +472,8 @@ static boolean_t emu_syscall(syscall_context_t *context, x86_thread_state64_t *s
 #undef SYSCALL4
 #undef SYSCALL5
 #undef SYSCALL6
+
+static kern_return_t emu_exn_ack(mach_port_t sysret_port);
 
 static kern_return_t emu_exn_wait(mach_port_t exn_set) {
         kern_return_t retval;
@@ -494,9 +502,10 @@ static kern_return_t emu_exn_wait(mach_port_t exn_set) {
                 .sysret_port = exn_req.hdr.msgh_remote_port,
                 .task = exn_req.task_port.name,
                 .thread = exn_req.thread_port.name,
+                .state = state,
         };
 
-        if(exn_req.exception == EXC_SYSCALL && emu_syscall(&context, state)) {
+        if(exn_req.exception == EXC_SYSCALL && emu_syscall(&context)) {
                 /* handled */
         } else {
                 const char* exn_str = "(unknown)";
@@ -569,10 +578,10 @@ nostack: ;
                         int j;
                         for(j = 0; j < insn_len; j++) {
                                 snprintf(bytes + (j % 8) * 3, 4, "%02x ", insns[j]);
-                                if(j > 0 && j % 8 == 7) {
+                                if(j > 0 && j % 8 == 7 && j < insn_len - 1) {
                                         DEBUG(" %s %016llx: %s", (i == 0 && j == 7 ? "=>" : "  "),
                                               rip + j - 7, bytes);
-                                        memset(bytes, 0x20, sizeof(bytes));
+                                        memset(bytes, 0x20, sizeof(bytes) - 1);
                                 }
                         }
                         memset(bytes + strlen(bytes), 0x20, sizeof(bytes) - strlen(bytes) - 1);
@@ -587,24 +596,25 @@ nostack: ;
                 LLVMDisasmDispose(Disasm);
 nodisasm: ;
 
-                task_terminate(context.task);
+                if(exn_req.exception == EXC_BREAKPOINT) {
+                        getchar();
+                        emu_exn_return(&context);
 
-                retval = KERN_FAILURE;
+                        retval = KERN_SUCCESS;
+                } else {
+                        if((retval = task_terminate(context.task)))
+                                FAIL("task_terminate", retval);
+
+                        retval = KERN_FAILURE;
+                }
         }
 
 fail:
         return retval;
 }
 
-static void emu_syscall_return(syscall_context_t *context, uint64_t syscall_retval,
-                               x86_thread_state64_t *state) {
+static kern_return_t emu_exn_return(syscall_context_t *context) {
         kern_return_t retval;
-
-        state->__rax = syscall_retval;
-
-        if((retval = thread_set_state(context->thread, x86_THREAD_STATE64, (natural_t*) state,
-                        sizeof(x86_thread_state64_t) / sizeof(natural_t))))
-                FAIL("thread_set_state", retval);
 
         struct {
                 mach_msg_header_t hdr;
@@ -612,35 +622,24 @@ static void emu_syscall_return(syscall_context_t *context, uint64_t syscall_retv
                 kern_return_t ret_code;
                 int flavor;
                 mach_msg_type_number_t state_len;
+                x86_thread_state64_t state;
         } __attribute__((packed)) exn_rep = {{0}};
         exn_rep.hdr.msgh_size = sizeof(exn_rep);
         exn_rep.hdr.msgh_remote_port = context->sysret_port;
         exn_rep.hdr.msgh_bits = MACH_MSGH_BITS_REMOTE(MACH_MSG_TYPE_MOVE_SEND_ONCE);
-        // TODO: kernel seems to ignore NDR, should we set it?
-        //exn_rep.NDR = exn_req.NDR;
+        exn_rep.hdr.msgh_id = 2507; /* reply to EXCEPTION_STATE_IDENTITY */
+        exn_rep.NDR = NDR_record;
         exn_rep.ret_code = KERN_SUCCESS;
-        // TODO: kernel seems to ignore returned state, wtf?
+        exn_rep.flavor = x86_THREAD_STATE64;
+        exn_rep.state_len = sizeof(exn_rep.state) / sizeof(natural_t);
+        memcpy(&exn_rep.state, context->state, sizeof(exn_rep.state));
         if((retval = mach_msg_send(&exn_rep.hdr)))
                 FAIL("mach_msg_send", retval);
 
-fail:   ; /* nothing */
+fail:   return retval;
 }
 
-static void emu_syscall_return_nostate(syscall_context_t *context, uint64_t syscall_retval) {
-        kern_return_t retval;
-        x86_thread_state64_t state;
-        mach_msg_type_number_t state_len;
-
-        if((retval = thread_get_state(context->thread, x86_THREAD_STATE64,
-                        (natural_t*) &state, &state_len)))
-                FAIL("thread_set_state", retval);
-
-        emu_syscall_return(context, syscall_retval, &state);
-
-fail:   ; /* nothing */
-}
-
-static kern_return_t emu_readelf(task_t task, thread_t thread, void *image) {
+static kern_return_t emu_loadelf(task_t task, thread_t thread, void *image) {
         kern_return_t retval = 0;
 
         Elf64_Ehdr *ehdr = image;
@@ -666,32 +665,16 @@ static kern_return_t emu_readelf(task_t task, thread_t thread, void *image) {
                         if(phdr->p_flags & PF_X)
                                 vm_prot |= VM_PROT_EXECUTE;
 
-                        if(phdr->p_filesz > 0) {
-                                vm_address_t source_addr = (vm_address_t) image + phdr->p_offset;
-                                vm_address_t target_addr = (vm_address_t) phdr->p_vaddr;
-                                // DEBUG("src %016lx dst %016lx sz %016llx",
-                                //       source_addr, target_addr, phdr->p_filesz);
+                        vm_address_t target_addr = (vm_address_t) phdr->p_vaddr;
+                        vm_address_t source_addr = (vm_address_t) image + phdr->p_offset;
 
-                                vm_prot_t cur, max;
-                                if((retval = vm_remap(task, &target_addr, phdr->p_filesz,
-                                                0, FALSE, mach_task_self(), source_addr,
-                                                TRUE, &cur, &max, VM_INHERIT_NONE)))
-                                        FAIL("vm_remap", retval);
-                        }
+                        ASSERT(phdr->p_memsz > 0);
 
-                        if(phdr->p_memsz > phdr->p_filesz) {
-                                vm_address_t target_addr = (vm_address_t)
-                                        i386_round_page(phdr->p_vaddr + phdr->p_filesz);
-                                vm_size_t delta = phdr->p_vaddr + phdr->p_filesz - target_addr;
-                                // DEBUG("dst %016lx delta %016lx sz %016llx",
-                                //       target_addr, delta, phdr->p_memsz - delta);
+                        if((retval = vm_allocate(task, &target_addr, phdr->p_memsz, FALSE)))
+                                FAIL("vm_allocate", retval);
 
-                                if(delta > 0) {
-                                        if((retval = vm_allocate(task, &target_addr,
-                                                        phdr->p_memsz - delta, FALSE)))
-                                                FAIL("vm_allocate:pt_load", retval);
-                                }
-                        }
+                        if((retval = vm_write(task, target_addr, source_addr, phdr->p_filesz)))
+                                FAIL("vm_write", retval);
 
                         if((retval = vm_protect(task, (vm_address_t) phdr->p_vaddr,
                                         phdr->p_memsz, FALSE, vm_prot)))
@@ -699,6 +682,9 @@ static kern_return_t emu_readelf(task_t task, thread_t thread, void *image) {
 
                         break;
                 }
+
+                case PT_NOTE:
+                        break;
 
                 case PT_GNU_STACK:
                         ASSERT(!(phdr->p_flags & PF_X));
@@ -712,7 +698,7 @@ static kern_return_t emu_readelf(task_t task, thread_t thread, void *image) {
 
         vm_size_t stack_size = 0x1000 * 64;
         vm_address_t stack_top = 0x7fff00000000, stack_addr = stack_top - stack_size;
-        DEBUG("stack dst %016lx sz %016lx", stack_addr, stack_size);
+        // DEBUG("stack dst %016lx sz %016lx", stack_addr, stack_size);
 
         if((retval = vm_allocate(task, &stack_addr, stack_size, FALSE)))
                 FAIL("vm_allocate:stack", retval);
@@ -724,6 +710,7 @@ static kern_return_t emu_readelf(task_t task, thread_t thread, void *image) {
         x86_thread_state64_t state = {0};
         state.__rip = ehdr->e_entry;
         state.__rsp = stack_top - 0x80; /* offset to make rsp printer work */
+        // state.__rflags = 0x100;
         if((retval = thread_set_state(thread, x86_THREAD_STATE64, (natural_t*) &state,
                         sizeof(x86_thread_state64_t) / sizeof(natural_t))))
                 FAIL("thread_set_state", retval);
@@ -731,29 +718,43 @@ static kern_return_t emu_readelf(task_t task, thread_t thread, void *image) {
         if((retval = thread_resume(thread)))
                 FAIL("thread_resume", retval);
 
+fail:   return retval;
+}
+
+static kern_return_t emu_readelf(task_t task, thread_t thread, const char *path) {
+        kern_return_t retval = 0;
+        void *image = NULL;
+        int fd = 0;
+
+        if((fd = open(path, O_RDONLY)) == -1)
+                UFAIL("open");
+
+        struct stat st;
+        if(fstat(fd, &st))
+                UFAIL("fstat");
+
+        if((image = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+                UFAIL("mmap");
+
+        close(fd);
+        fd = 0;
+
+        if((retval = emu_loadelf(task, thread, image)))
+                FAIL("emu_loadelf", retval);
+
 fail:
+        if(image && munmap(image, st.st_size))
+                XUFAIL("munmap");
+
+        if(fd)
+                close(fd);
+
         return retval;
 }
 
 static void happy_dance(const char *init) {
-        kern_return_t retval, xretval;
+        kern_return_t retval;//, xretval;
         mach_port_t exn_set = 0;
-        int ret, xret;
-        int fd = 0;
-        void *image = NULL;
-
-        if(!(fd = open(init, O_RDONLY)))
-                UFAIL("open", fd);
-
-        struct stat st;
-        if((ret = fstat(fd, &st)))
-                UFAIL("fstat", ret);
-
-        if((image = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
-                UFAIL("mmap", errno);
-
-        close(fd);
-        fd = 0;
 
         if((retval = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_PORT_SET, &exn_set)))
                 FAIL("mach_port_allocate", retval);
@@ -765,7 +766,7 @@ static void happy_dance(const char *init) {
         if((retval = emu_blank_slate(exn_set, &task, &thread)))
                 FAIL("emu_blank_slate", retval);
 
-        if((retval = emu_readelf(task, thread, image)))
+        if((retval = emu_readelf(task, thread, init)))
                 FAIL("emu_readelf", retval);
 
         while(TRUE) {
@@ -773,15 +774,9 @@ static void happy_dance(const char *init) {
                         FAIL("emu_exn_wait", retval);
         }
 
-fail:
-        if(exn_set && (xretval = mach_port_deallocate(mach_task_self(), exn_set)))
-                XFAIL("mach_port_deallocate:exn_set", xretval);
-
-        if(image && (xret = munmap(image, st.st_size)))
-                XUFAIL("munmap", xret);
-
-        if(fd)
-                close(fd);
+fail: ;
+        // if(exn_set && (xretval = mach_port_deallocate(mach_task_self(), exn_set)))
+        //         XFAIL("mach_port_deallocate:exn_set", xretval);
 }
 
 int main(int argc, char** argv) {
